f7582491d0877022dca06aa9cb1de9a65db09d11
wangji <And3b6s5y>
2016-06-15 20:55:23 +0800

    Add test case

--- intellitags-files/src/main/java/net/tinybrick/intellitags/controllers/FileController.java
+++ intellitags-files/src/main/java/net/tinybrick/intellitags/controllers/FileController.java
@@ -7,16 +7,20 @@ import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.ws.rs.QueryParam;
+import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wangji on 2016/6/15.
  */
 @RestController
 @RequestMapping("/v1.0")
-@Api(value = "有关文件传送的API", tags = {"版本v1.0"}, basePath= "/v1.0")
+@Api(value = "有关传送文件的API", tags = {"版本v1.0"}, basePath= "/v1.0")
+//@ApiDoc
 public class FileController {
     private static Logger logger = Logger.getLogger(FileController.class);
 
@@ -66,12 +70,93 @@ public class FileController {
     @ApiOperation(value = "检查文件偏移量",
             notes = "如果发生文件传送中断，可以通过这个方法查寻文件当前的偏移量，程序将返回已接收文件的长度。<p>"
                     + "<br>输入 ticket"
-                    + "<br>输出 offset")
+                    + "<p>输出 offset")
     @RequestMapping(value = {"/file/offset/{ticket}"},
             method = {RequestMethod.GET},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
     public Integer queryOffset(@PathVariable("ticket") String ticket){
         return 1024;
+    }
+
+    /**
+     * 上载单个文件
+     *
+     * @param files
+     * @param ticket
+     * @return
+     * @throws IllegalStateException
+     * @throws IOException
+     */
+    @ApiOperation(value = "上载单个文件",
+            notes = "这个接口一次只接受一个文件。ticket参数为可选，如果不提供ticket，系统将不支持断点续传。<p>"
+                    + "<br>输入 file"
+                    + "<br>输入 ticket（可选）如果上载的时候提供了 ticket，并且文件上载成功，没有发生意外，零时ticket将自动销毁"
+                    + "<p>输出 上载成功后返回文件ID。")
+    @RequestMapping(value = "/file",
+            method = RequestMethod.POST,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    @ResponseBody
+    public String uploadSingleFile(@RequestParam(value="file",required = false) MultipartFile files, @RequestParam(value="ticket",required = false) String ticket)
+            throws IllegalStateException, IOException {
+        return null;
+    }
+
+    /**
+     *
+     * @param files
+     * @param ticket
+     * @return
+     */
+    @ApiOperation(value = "追加上载单个文件",
+            notes = "这个接口一次只接受对一个文件的追加上载，并且ticket参数为必须，<p>"
+                    + "<br>输入 file"
+                    + "<br>输入 ticket 如果上载结束前没有发生意外，零时ticket将自动销毁"
+                    + "<p>输出 上载成功后返回文件ID。")
+    @RequestMapping(value = "/file/{ticket}",
+            method = RequestMethod.PATCH,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    @ResponseBody
+    public String patchSingleFile(@RequestParam("file") MultipartFile files, @PathVariable String ticket) {
+        return null;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    @ApiOperation(value = "删除单个文件",
+            notes = "这个接口一次只接受对一个文件的删除<p>"
+                    + "<br>输入 id")
+    @RequestMapping(value = "/file/{id}",
+            method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFile( @PathVariable("id") String id) {
+        ;
+    }
+
+    /**
+     * 同时上载多个文件
+     *
+     * @param files
+     * @return
+     * @throws IllegalStateException
+     * @throws IOException
+     */
+    @ApiOperation(value = "同时上载多个文件",
+            notes = "这个接口接受同时上载多个文件，但不支持断点续传。<p>"
+                    + "<br>输入 file"
+                    + "<p>输出 上载成功后返回文件ID和文件名的对照表。")
+    @RequestMapping(value = "/files",
+            method = RequestMethod.POST,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    @ResponseBody
+    public Map<String, String> uploadFiles(@RequestParam("file") MultipartFile[] files)
+            throws IllegalStateException, IOException {
+        Map<String, String> fileMap = new HashMap<String, String>();
+        return fileMap;
     }
 }
---
+++ intellitags-files/src/test/java/net/tinybrick/intellitags/controllers/unit/FileControllerTestCases.java
@@ -0,0 +1,42 @@
+package net.tinybrick.intellitags.controllers.unit;
+
+import net.tinybrick.intellitags.IntellitagsMain;
+import net.tinybrick.intellitags.configuration.IntellitagsConfigure;
+import net.tinybrick.web.configure.ApplicationCoreConfigure;
+import net.tinybrick.web.unit.ControllerTestBase;
+import org.junit.Test;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by wangji on 2016/6/15.
+ */
+@SpringApplicationConfiguration(classes = IntellitagsMain.class)
+//@TestPropertySource(locations = "classpath:config/test.properties")
+public class FileControllerTestCases extends ControllerTestBase{
+    @Test
+    public void testGetTicket() throws Exception {
+        ResultActions resultActions;
+        resultActions = GET("/v1.0/ticket", MediaType.ALL, MediaType.APPLICATION_JSON);
+        resultActions.andDo(print()).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testPostSingleFile() throws Exception {
+        MockMultipartFile mockFile = new MockMultipartFile("file", "filename.txt", "text/plain", "some data".getBytes());
+
+        ResultActions resultActions =  mockMvc.perform(fileUpload("/v1.0/file").file(mockFile).session(session)
+                .contentType(MediaType.MULTIPART_FORM_DATA).accept(MediaType.APPLICATION_JSON));
+        resultActions.andDo(print()).andExpect(status().isCreated());
+    }
+}
